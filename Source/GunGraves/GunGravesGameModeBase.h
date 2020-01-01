8322ace3f24b5d361a1b2fc2a24fd3358b5f5142
Chris Whiting <mpro34@gmail.com>
2019-12-31 16:59:31 -0800

    First Commit

---
+++ Source/GunGraves/GunGravesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GunGravesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNGRAVES_API AGunGravesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
