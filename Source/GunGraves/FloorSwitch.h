9ed2abd56cd53b031d2b35f4b4fc98aa79730e4c
Chris Whiting <mpro34@gmail.com>
2020-01-05 07:27:27 -0600

    Add floating platform with interpolation

---
+++ Source/GunGraves/FloorSwitch.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloorSwitch.generated.h"
+
+UCLASS()
+class GUNGRAVES_API AFloorSwitch : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloorSwitch();
+
+	/** Overlap volume for functionality to be triggered */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+	class UBoxComponent* TriggerBox;
+
+	/** Switch for the character to step on */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+	class UStaticMeshComponent* FloorSwitch;
+
+	/** Door to move when the floor switch is stepped on. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
+	UStaticMeshComponent* Door;
+
+	// Initial location for the door
+	UPROPERTY(BlueprintReadWrite, Category = "Floor Switch")
+	FVector InitialDoorLocation;
+
+	// Initial location for the floor switch
+	UPROPERTY(BlueprintReadWrite, Category = "Floor Switch")
+	FVector InitialSwitchLocation;
+
+	FTimerHandle SwitchHandle;
+
+	UPROPERTY(EditAnywhere, Category = "Floor Switch")
+	float SwitchTime;
+
+	bool bCharacterOnSwitch;
+
+	void CloseDoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void RaiseDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void LowerDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void LowerFloorSwitch();
+
+	// Declared in C++, implemented in Blueprints
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void RaiseFloorSwitch();
+
+	// Declared and implementated in C++
+	UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+	void UpdateDoorLocation(float z);
+
+	UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+	void UpdateFloorSwitchLocation(float z);
+};
